ea9d475f19dc202f53c5961b0897022f6fa73df9
fnodado <francisdahryljava@gmail.com>
2022-11-16 11:51:15 +0800

    added implementation for event driven kafka microservices

---
+++ base-domains/src/test/java/net/ripe/basedomains/BaseDomainsApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ripe.basedomains;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseDomainsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
